01f8d7699615291d49cdcef7cad71cfd40008258
zhangyubo_sx <zhangyubo01_sx@qiyi.com>
2026-02-11 14:07:36 +0800

    [RabbitMQ-0006] 死信队列消息消费失败实战

---
+++ src/main/java/com/zyb/rabbitmq_learning/workers/DeadWorker02.java
@@ -0,0 +1,29 @@
+package com.zyb.rabbitmq_learning.workers;
+
+import com.rabbitmq.client.CancelCallback;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import com.zyb.rabbitmq_learning.Util.RabbitMQUtils;
+
+public class DeadWorker02 {
+    public static final String DEAD_QUEUE = "dead_queue";
+
+    public static void main(String[] args) throws Exception {
+
+        Channel channel = RabbitMQUtils.getChannel();
+
+        System.out.println("等待接收消息.....");
+
+        //接收消息
+        //消息消费时的回调函数
+        DeliverCallback deliverCallback = (consumerTag, message) -> {
+            System.out.println("Consumer02接收的消息是：" + new String(message.getBody(), "UTF-8"));
+        };
+        //消息取消时的回调函数
+        CancelCallback cancelCallback = consumerTag -> {
+
+        };
+        // 仅消费死信队列的东西
+        channel.basicConsume(DEAD_QUEUE, true, deliverCallback, cancelCallback);
+    }
+}
